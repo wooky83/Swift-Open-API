9f0fb18a0ddf2b1f677cfc7bfe6ec01ffae096eb
1002659 <baw0803@sk.com>
2017-10-23 17:19:49 +0900

    [Dev] RxSwift, RxCoCoa Example Code를 위한 ViewController 생성

--- SwiftReference/ViewController/MainVC.swift
+++ SwiftReference/ViewController/MainVC.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class MainVC: UIViewController {
     
+    enum Row {
+        case rxSwift(String)
+        case rxCocoa(String)
+    }
+    var topicArray: [Row] = []
+    @IBOutlet weak var mainTableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        topicArray.append(Row.rxSwift("rxSwift"))
+        topicArray.append(Row.rxCocoa("rxCoCoa"))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,37 @@ class MainVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension MainVC: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return topicArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        switch topicArray[indexPath.row] {
+        case .rxSwift(let title):
+            cell.textLabel?.text = title
+        case .rxCocoa(let title):
+            cell.textLabel?.text = title
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        switch topicArray[indexPath.row] {
+        case .rxSwift(let title):
+            performSegue(withIdentifier: title, sender: nil)
+        case .rxCocoa(let title):
+            performSegue(withIdentifier: title, sender: nil)
+        }
+        
+    }
+    
 }
 
---
+++ SwiftReference/ViewController/RxCoCoaVC.swift
@@ -0,0 +1,23 @@
+//
+//  RxCoCoaVC.swift
+//  SwiftReference
+//
+//  Created by 1002659 on 2017. 10. 23..
+//  Copyright © 2017년 wooky83. All rights reserved.
+//
+
+import UIKit
+
+class RxCoCoaVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
