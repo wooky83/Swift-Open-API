e0183af11c3a69f03e5abdfee54dce60579e22f4
1002659 <baw0803@sk.com>
2019-01-04 11:07:30 +0900

    [Dev] RxSwiftExt - Pairwise, mapAt

---
+++ SwiftReference/Playground/Rx_Operator/Ex_RxSwiftExt.playground/Contents.swift
@@ -0,0 +1,66 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+import RxOptional
+import RxSwiftExt
+import PlaygroundSupport
+
+let disposeBag = DisposeBag()
+
+
+print("Pairwise")
+/*
+ ----(0)----(1)----(2)----(3)-----------------------
+ Pairwise
+ -----------(0,1)--(1,2)--(2,3)-------------------
+ */
+
+do {
+    let subject = BehaviorSubject<Int>(value: 0)
+    
+    subject.asObserver()
+        .pairwise()
+        .subscribe(onNext: { pre, next in
+            print("pre : \(pre), current: \(next)")
+        })
+        .disposed(by: disposeBag)
+    
+    subject.onNext(1)
+    subject.onNext(2)
+    subject.onNext(3)
+    subject.onNext(4)
+}
+
+print("------------------------------------------------------------")
+print("mapAt")
+//keyPath를 이용해서 struct, class의 Value를 Subscribe한다
+
+/*
+ ------(0,*)------(1,$)------(2,?)--------------
+ mapAt
+ ------(*)--------($)--------(?)----------------
+ */
+
+do {
+    struct Sport {
+        let name: String
+        let rate: Int
+        let tag: String
+    }
+    
+    let sj = PublishSubject<Sport>()
+    
+    sj.asObserver()
+        .mapAt(\.tag)
+        .subscribe(onNext: { value in
+            print(value)
+        })
+        .disposed(by: disposeBag)
+    
+    sj.onNext(Sport(name: "baseBall", rate: 0, tag: "*"))
+    sj.onNext(Sport(name: "Soccer", rate: 1, tag: "$"))
+    sj.onNext(Sport(name: "Golf", rate: 2, tag: "?"))
+}
+
+PlaygroundPage.current.needsIndefiniteExecution = true
