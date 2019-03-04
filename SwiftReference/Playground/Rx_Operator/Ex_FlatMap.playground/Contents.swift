60fd504d1dac2c28edd19b7cd119402a51fce89c
1002659 <baw0803@sk.com>
2019-03-04 15:18:48 +0900

    [Ref] Rxswift FlatMap Example

---
+++ SwiftReference/Playground/Rx_Operator/Ex_FlatMap.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import RxSwift
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+
+print("FlatMap")
+/*
+ */
+
+do {
+    
+    struct Student {
+        var score: Variable<Int>
+    }
+    
+    let sung = Student(score: Variable(99))
+    let wook = Student(score: Variable(80))
+    
+    let student = PublishSubject<Student>()
+    
+    student
+        .flatMap {
+            $0.score.asObservable()
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+    
+    student.onNext(sung)
+    sung.score.value = 90
+    student.onNext(wook)
+    wook.score.value = 9
+}
+
